46fc8ac7bd68001c60bd86235e9c3b01b8321ed8
ornanoxzpp-code <ornanoxzpp@gmail.com>
2025-10-10 15:34:08 +0700

    Update m2-script.js

--- m2-script.js
+++ m2-script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     // *** ✅ CONFIG: URL ของ Apps Script ที่คุณ Deploy มาจาก Google Sheet ม.1 (อันล่าสุด) ✅ ***
-    const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyr0-tU9g6yiZ1ReZT1C6PtvguddPLuM1ihvkZXIrzPHDKvihgVPQ1r1EInJTRVubNljg/exec'; 
+    const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzzH1Nv5vooTmiC9kujlEibIYnhZmrqL18sNlSkf6REMvj-x4B-q5Igm4bxNTbUOYFsRg/exec'; 
     
     // ------------------------------------------------------------------
     // เลือกองค์ประกอบที่ใช้ในการทำงาน
